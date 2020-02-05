ac5fd8dff9826cc20a9296b2457cfed943f9c505
tmax818 <tylermaxwell661@gmail.com>
2020-02-04 16:25:01 -0800

    init commit

---
+++ app.txt
@@ -0,0 +1,10 @@
+console.log("src/app.js is running")
+
+
+const app = (
+    <div>
+        hi
+    </div>
+);
+
+ReactDOM.render(app, document.getElementById('app'));
---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("src/app.js is running");
+
+var app = React.createElement(
+    "div",
+    null,
+    "hi"
+);
+
+ReactDOM.render(app, document.getElementById('app'));
